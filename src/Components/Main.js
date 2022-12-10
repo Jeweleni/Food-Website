16ab7a35b5310a8b167db97f71b953d4ec01c382
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2022-12-10 09:49:53 +0100

    1st commit

---
+++ src/Components/Main.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Meal = ()=> {
+    return(
+        <>
+           <div className="main">
+            <div className="heading">
+                <h1>Find a Meal</h1>
+                <h4>lorem, ipsum dolor sit amet consectetur adipiscing elit. placeat veritatis dictum error qui</h4>
+
+            </div>
+            <div className="searchbox">
+                <input type="search" className="searchbar"></input>
+
+            </div>
+
+           </div>
+        </>
+    )
+}
+export default Meal
