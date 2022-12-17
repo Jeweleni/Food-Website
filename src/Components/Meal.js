6e91e4c1e7cfe4e7903dcb10309d0980c4b15d70
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2022-12-17 12:00:25 +0100

    api push

--- src/Components/Meal.js
+++ src/Components/Meal.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./style.css";
 import MealType from "./MealType";
-import Recipe from "./Recipe";
+import RecipeIndex from "./RecipeIndex";
 
 
 const Meal = ()=> {
     const[url,setUrl]= useState('https:/www.themealdb.com/api/json/v1/1/search.php?f=a');
     const[item, setItem]= useState();
     const[show, setShow]= useState(false);
+    const[search, setSearch]=useState("")
     useEffect(()=>{
         fetch(url).then(res=>res.json()).then(data=>{
             console.log(data.meals);
@@ -16,7 +17,16 @@ const Meal = ()=> {
         })
     },[url])
 
-    const setRecipe=(alpha)=>{}
+    const setIndex=(alpha)=>{
+       setUrl(`https:/www.themealdb.com/api/json/v1/1/search.php?f=${alpha}`)
+    }
+
+    const searchRecipe=(evt)=>{
+        if(evt.key=="Enter"){
+            setUrl(`https:/www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+        }
+    }
+
     return(
      
         <div className="main">
@@ -26,7 +36,8 @@ const Meal = ()=> {
 
             </div>
             <div className="searchbox">
-                <input type="search" className="searchbar"/>
+                <input type="search" className="searchbar"
+                 onChange={e=>setSearch(e.target.value)} onKeyPress={searchRecipe}/>
                 
 
             </div>
@@ -37,7 +48,7 @@ const Meal = ()=> {
                   
             </div>
             <div className="recipeContainer">
-                  <Recipe alphaRecipe={(alpha)=>setRecipe(alpha)}/>
+                  <RecipeIndex alphaIndex={(alpha)=>setIndex(alpha)}/>
             </div>
         </div>
        
--- src/Components/RecipeIndex.js
+++ src/Components/RecipeIndex.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Recipe =({alphaRecipe})=>{
+const Recipe =({alphaIndex})=>{
     const alpha=["A", "B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     var num=0;
     return(
@@ -8,7 +8,7 @@ const Recipe =({alphaRecipe})=>{
             {
                 alpha.map(item=>{
                     return(
-                        <div className="numBox" key={num++} onClick={()=>alphaRecipe(item)}>
+                        <div className="numBox" key={num++} onClick={()=>alphaIndex(item)}>
                             <h3>{item}</h3>
                         </div>
                     )
